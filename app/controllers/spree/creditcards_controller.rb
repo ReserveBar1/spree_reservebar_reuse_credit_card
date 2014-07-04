3667ce5c7ac9b3ec39f7afba62b69129ab7e9445
Kenton Newby <kentonnewby@gmail.com>
2014-07-03 22:25:19 -0500

    updates destroy action to soft-delete matching credit cards across all retailers when user deletes a credit card

--- app/controllers/spree/creditcards_controller.rb
+++ app/controllers/spree/creditcards_controller.rb
@@ -5,18 +5,39 @@ module Spree
 
     def destroy
       @creditcard = Spree::Creditcard.find(params["id"])
+      @user = @creditcard.user
       authorize! :destroy, @creditcard
 
       # TODO: think about the necessity of deleting payment profiles here.
       # I'm thinking we want to always leave them alone
 
-      @creditcard.deleted_at = Time.now
       if @creditcard.save
+        delete_matching_credit_cards
         respond_with @creditcard
       else
         render template: 'spree/creditcards/destroy_error'
       end
 
     end
+
+    private
+
+    def matching_credit_cards
+      conditions = {:month => @creditcard.month,
+                    :year => @creditcard.year,
+                    :cc_type => @creditcard.cc_type,
+                    :last_digits => @creditcard.last_digits,
+                    :first_name => @creditcard.first_name,
+                    :deleted_at => nil}
+      @user.creditcards.find(:all, :conditions => conditions)
+    end
+
+    def delete_matching_credit_cards
+      matching_credit_cards.each do |cc|
+        cc.deleted_at = Time.now
+        cc.save
+      end
+    end
   end
+
 end
