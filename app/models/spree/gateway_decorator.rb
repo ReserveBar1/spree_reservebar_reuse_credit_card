fd2b2cc277097afb7e9d792a8afe16e3654021c9
Jason Knebel <jasonkneb@gmail.com>
2015-03-17 22:10:02 -0400

    adding BraintreeGateway to gateways

--- app/models/spree/gateway_decorator.rb
+++ app/models/spree/gateway_decorator.rb
@@ -1,6 +1,17 @@
 Spree::Gateway::AuthorizeNet.class_eval do
-  attr_accessible :name, :description, :environment, :display_on, :active, :type, :deleted_at, :preferred_test, :preferred_server, :preferred_test_mode
+  attr_accessible :name, :description, :environment, :display_on,
+    :active, :type, :deleted_at, :preferred_test, :preferred_server,
+    :preferred_test_mode
 end
+
 Spree::Gateway::AuthorizeNetCim.class_eval do
-  attr_accessible :name, :description, :environment, :display_on, :active, :type, :deleted_at, :preferred_test, :preferred_server, :preferred_test_mode
+  attr_accessible :name, :description, :environment, :display_on,
+    :active, :type, :deleted_at, :preferred_test, :preferred_server,
+    :preferred_test_mode
+end
+
+Spree::Gateway::BraintreeGateway.class_eval do
+  attr_accessible :name, :description, :environment, :display_on,
+    :active, :type, :deleted_at, :preferred_test, :preferred_server,
+    :preferred_test_mode
 end
