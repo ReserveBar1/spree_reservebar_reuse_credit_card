d461be3e7b9b28d63aa337f93f0c7f0ef8496a01
Jason Knebel <jasonkneb@gmail.com>
2015-03-24 18:43:15 -0400

    adding billing address to source_params

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -11,17 +11,6 @@ Spree::CheckoutController.class_eval do
     @cards = all_cards_for_user(@order.user, @order.retailer)
   end
   
-  # Set the credit cards address to the order's bill address, if the credit card does not yet 
-  # have the bill address set. This should only happen when the user enters a new credit card.
-  # Normally, this should happen in after_payment, but that somehow gets called one step too early, after the delivery step.
-  # So we push it to the before_confirm callback instead.
-  def before_confirm
-    source = current_order.payment.source
-    if source.is_a?(Spree::Creditcard) && (source.address_id == nil)
-      source.update_attribute(:address_id, current_order.bill_address_id)
-    end
-  end
-
   # we are overriding this method in order to substitue in the exisiting card information
   # since we are also storing the billing address with the existing credit cards, we need to get it from the credit card
   # and dump it into the order:bill_address_id
@@ -29,6 +18,7 @@ Spree::CheckoutController.class_eval do
     # For payment step, filter order parameters to produce the expected nested attributes for a single payment and its source, discarding attributes for payment methods other than the one selected
     if @order.payment?
       if params[:payment_source].present? && source_params = params.delete(:payment_source)[params[:order][:payments_attributes].first[:payment_method_id].underscore]
+        source_params[:address_id] = params[:order][:bill_address_id]
         if params[:existing_card]
           creditcard = Spree::Creditcard.find(params[:existing_card])
           # at this point the user may have selected a credit card that is tokenized on another retailer's account
